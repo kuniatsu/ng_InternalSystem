815ff86e007a9d7857c50fb187e45e798b073644
kuniatus <nightmaredes@hotmail.com>
2017-05-21 22:38:59 +0900

    予算一覧表示作成　調整が必要

--- src/app/yosan-list/yosan-list.component.ts
+++ src/app/yosan-list/yosan-list.component.ts
@@ -1,4 +1,9 @@
+///<reference path="../../../node_modules/@angular/http/src/base_request_options.d.ts"/>
 import { Component, OnInit } from '@angular/core';
+import {Kinmudata} from "../yosan/yosan.kinmudata";
+import {Request, Response, RequestOptions, URLSearchParams, Headers, Http} from "@angular/http";
+import {Router} from "@angular/router";
+import {YosanListData} from "./yosanListData";
 
 @Component({
   selector: 'app-yosan-list',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YosanListComponent implements OnInit {
 
-  constructor() { }
+  kinmudataArray:Kinmudata[]=[];//サンプル
+  yosanListData:YosanListData[]=[];
+  constructor(
+    private http: Http,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+    this.updateStatus();//json読み込み
+  }
+
+
+
+  //json読み取り
+  updateStatus() {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let params = new URLSearchParams();
+    params.set('start', '2016');
+    params.set('end', '2017');
+    let options = new RequestOptions({ headers: headers, search: params});
+
+
+
+    //get送信
+    this.http.request(new Request({
+      method: "Get",
+      url: "./yosan.json"
+    }),options).subscribe((res: Response) => {
+      console.log("get送信内");
+      console.dir(res);
+      var jsonObj = res.json();
+      for (var key in jsonObj) {
+        var jsonDoc = jsonObj[key];
+        // this.kinmudataArray.push(new Kinmudata(jsonDoc["start"], jsonDoc["name"], jsonDoc["value"]).changeHyphen());
+        this.yosanListData.push(new YosanListData(
+          jsonDoc["code"],
+          jsonDoc["status"],
+          jsonDoc["name"],
+          jsonDoc["start"],
+          jsonDoc["end"],
+          jsonDoc["value"],
+          jsonDoc["koutsuuhi"],
+          jsonDoc["zangyou"],
+          jsonDoc["value_nokori"],
+          jsonDoc["koutsuuhi_nokori"],
+          jsonDoc["zangyou_nokori"]
+        ));
+
+
+      }
+    },error => {
+      alert(error);
+    });
+
   }
 
 }
---
+++ src/app/yosan-list/yosanListData.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by kuniatsu on 2017/05/21.
+ */
+export class YosanListData{
+  constructor(
+    private code:string,
+    private status:string,
+    private name:string,
+    private start:string,
+    private end:string,
+    private value:string,
+    private koutsuuhi:string,
+    private zangyou:string,
+    private value_nokori:string,
+    private koutsuuhi_nokori:string,
+    private zangyou_nokori:string
+
+  ){}
+
+
+}
